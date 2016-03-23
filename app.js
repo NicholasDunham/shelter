6d7f56cf2e28dddeb1078a0cb0e58c275a4ac24a
nicholasdunham <nmdnhm@gmail.com>
2016-03-23 12:07:03 -0700

    Configure MongoDB connection (using MongoLab)

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+mongoose.connect(process.env.MONGOLAB_URI, function(err) {
+  if (err) {
+    console.error('Error connecting to MongoDB:', err);
+  } else {
+    console.log('Connected to MongoDB.');
+  }
+});
 
 var app = express();
 
@@ -14,16 +20,15 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/', require('./routes/index'));
+app.use('/clients', require('./routes/clients'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
