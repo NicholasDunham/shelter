88688f5591da8f90ee37f60916381b90d384dabb
nicholasdunham <nmdnhm@gmail.com>
2016-03-23 12:07:34 -0700

    Add basic get and post routes for clients

--- routes/clients.js
+++ routes/clients.js
@@ -1,9 +1,18 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const Client = require('../models/client');
+
+router.route('/')
+  .get(function(req, res) {
+    Client.find({}, function(err, clients) {
+      res.status(err ? 400 : 200).send(err || clients);
+    });
+  })
+  .post(function(req, res) {
+    Client.create(req.body, function(err, newClient) {
+      res.status(err ? 400 : 200).send(err || newClient);
+    });
+  });
 
 module.exports = router;
